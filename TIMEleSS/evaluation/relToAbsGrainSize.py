2fe348415a0c8ccdf434375ff35b38e006b9f90b
smerkel <sebastien.merkel@univ-lille.fr>
2023-01-16 15:05:53 +0100

    Better output with no confusion between radius and volume

--- TIMEleSS/evaluation/relToAbsGrainSize.py
+++ TIMEleSS/evaluation/relToAbsGrainSize.py
@@ -46,36 +46,47 @@ import numpy
 # rotationrange:                Full rotation range used in the experiment in $\mu$m. Necessary to calculate the illuminated sample volume more accurately.
 # samplethickness:              Thickness of the sample in $\mu$m. Necessary to calculate the illuminated sample volume.
 # indexquality:                 Quality of the previous indexing process in percent. Used to account for the fact that not all grains were found during the indexing, between 0 and 1.
-# radius:                       Boolean operator that determines if grainsizelist consists of grain volumes or grain radii. True means volumes, False means radii.
 # histogram_bins:               Number of histogram bins you want to use. By default, no histogram is plotted.
 # proportion:                   Defines how much of the sample is your phase of interest. Can be between 0 and 1.
 
-def absolute_grainsizes(grainsizelist, beamsize_H, beamsize_V, rotationrange, samplethickness, indexquality, radius=True, proportion=1.0):
-	with open(grainsizelist) as g:
-		grainsizes = g.readlines()
-	total = 0
-	ngrains = 0
-	grainRelVolumes = []
-	for grain in grainsizes:
-		if not grain.strip().startswith("#"):
-			ngrains += 1
-			grain = float(grain)
-			if (radius): # Grain size in file is a radius
-				grainV = 4./3*numpy.pi*grain**(3.) # Turn grain radii into grain volumes
-				grainRelVolumes.append(grainV)
-			else: 
-				grainV = grain
-				grainRelVolumes.append(grainV)
-			total += grainV
+def absolute_grainsizes(grainsizelist, beamsize_H, beamsize_V, rotationrange, samplethickness, indexquality, proportion=1.0):
+	# Read relative grain sizes
+	# Read file
+	f = open(grainsizelist, 'r')
+	lines = f.readlines()
+	f.close()
+	relativeVols = []
+	relativeRads = []
+	grainID = []
+	eulers = []
+	lin = 0
+	for line in lines:
+		li=line.strip()
+		lin += 1
+		if not li.startswith("#"):
+			litxt = li.split()
+			if (len(litxt) == 2):
+				print("ERROR: It seems that you have a old file format, re-run timelessExtractGrainSizes to generate a new file with relative grain sizes")
+				return
+			else:
+				if (len(litxt) != 6):
+					print("ERROR: Something is wrong with line %i: %s")
+					return
+				grainID.append(int(litxt[0]))
+				eulers.append([float(litxt[1]), float(litxt[2]), float(litxt[3])])
+				relativeVols.append(float(litxt[4]))
+				relativeRads.append(float(litxt[5]))
+	
+	total = sum(relativeVols)
 
 	# Calculate the sample chamber volume. Check the wiki for more info on the formula.
 	totalsamplechambervol = beamsize_V * beamsize_H * samplethickness * numpy.cos(rotationrange*numpy.pi/180/2) + 0.5 * beamsize_V * samplethickness**2 * numpy.tan(rotationrange*numpy.pi/180/2)
 	indexedGrainsV = totalsamplechambervol * indexquality * proportion # Account for the indexing quality and side phases
 	print("Read arbitrary grain sizes from %s" % grainsizelist)
-	if (radius):
-		print("I believe I read a list of grain radii")
-	else:
-		print("I believe I read a list of grain volumes")
+	#if (radius):
+	#	print("I believe I read a list of grain radii")
+	#else:
+	#	print("I believe I read a list of grain volumes")
 	print("Beam size H x V (µm): %.1f, %.1f" % (beamsize_H, beamsize_V))
 	print("Sample thickness (µm): %.1f" % (samplethickness))
 	print("Rotation range (degrees): %.1f" % (rotationrange))
@@ -84,44 +95,37 @@ def absolute_grainsizes(grainsizelist, beamsize_H, beamsize_V, rotationrange, sa
 	print("Total illuminated sample chamber volume (µm^3): %.1f" % totalsamplechambervol)
 	print("Total volume of indexed grains (µm^3): %.1f" % indexedGrainsV)
 	print("Total volume of indexed grains (arbitrary unit): %.1f" % total)
-	print("Number of grains: %i" % ngrains)
+	print("Number of grains: %i" % len(relativeVols))
 	
 	ratio_V = indexedGrainsV / total # How many µm^3 equals one relative grain size unit
 	
-	# Create a new file that contains the absolute grain size 
-	newfile = grainsizelist[:-4] + "_abs.txt"
-	filename, file_extension = os.path.splitext(grainsizelist)
-	grainsizes_R = [] # Make a list of the new grain sizes
-	grainsizes_V = [] # Make a list of the new grain sizes
-	stringV = ""
-	stringR = ""
-	for grainRelV in grainRelVolumes:
+	# Calculate absolute grain sizes
+	absRads = [] # Make a list of the new grain sizes
+	absVols = [] # Make a list of the new grain sizes
+	for grainRelV in relativeVols:
 		grainV = grainRelV * ratio_V
 		grainR = (grainV*3./(4.*numpy.pi))**(1./3)
-		stringV += str(grainV) + "\n"
-		stringR += str(grainR) + "\n"
-		grainsizes_R.append(grainR)
-		grainsizes_V.append(grainV)
-	newfile = filename + "_absV.txt"
+		absRads.append(grainR)
+		absVols.append(grainV)
+	
+	# Create a new file that contains the absolute grain size 
+	filename, file_extension = os.path.splitext(grainsizelist)
+	newfile = filename + "_abs.txt"
 	f= open(newfile,"w+")
-	f.write("# Absolute grain volumes (µm^3)\n")
-	f.write(stringV)
+	f.write("# grainID, phi1, Phi, phi2, relative grain volume, grain radii (arbitrary unit), absolute volume (µm^3), absolute radii (µm)\n")
+	for i in range(0,len(relativeVols)):
+		f.write("%i %.2f %.2f %.2f %.4e %.4e %.4e %.4e\n" % (grainID[i], eulers[i][0], eulers[i][1], eulers[i][2], relativeVols[i], relativeRads[i], absVols[i], absRads[i]))
 	f.close()
-	print ("\nSaved new list of grain volumes (in µm^3) as %s." % (newfile))
-	newfile = filename + "_absR.txt"
-	f= open(newfile,"w+")
-	f.write("# Absolute grain radii (µm)\n")
-	f.write(stringR)
-	f.close()
-	print ("Saved new list of grain radii (in µm) as %s." % (newfile))
+	print ("\nSaved new list of grain radii and volumes (in µm and µm^3) as %s." % (newfile))
 	
 	# Print out some statistics
-	print ("\nGrain volumes:\n\tmean: %d\n\tmedian: %d\n\tmax: %d\n\tmin: %d" % (numpy.mean(grainsizes_V), numpy.median(grainsizes_V), numpy.min(grainsizes_V), numpy.max(grainsizes_V)))
-	print ("\nGrain radii:\n\tmean: %.2f\n\tmedian: %.2f\n\tmax: %.2f\n\tmin: %.2f" % (numpy.mean(grainsizes_R), numpy.median(grainsizes_R), numpy.min(grainsizes_R), numpy.max(grainsizes_R)))
+	print ("\nNumber of grains: %d" % len(absVols))
+	print ("\nGrain volumes (µm^3):\n\tmean: %.2f\n\tmedian: %.2f\n\tmin: %.2f\n\tmax: %.2f" % (numpy.mean(absVols), numpy.median(absVols), numpy.min(absVols), numpy.max(absVols)))
+	print ("\nGrain radii (µm):\n\tmean: %.2f\n\tmedian: %.2f\n\tmin: %.2f\n\tmax: %.2f" % (numpy.mean(absRads), numpy.median(absRads), numpy.min(absRads), numpy.max(absRads)))
 	
 	# Return grain radii
-	med = numpy.median(grainsizes_R)
-	return grainsizes_R, med
+	med = numpy.median(absRads)
+	return absRads, med
 
 
 
@@ -156,7 +160,7 @@ This is part of the TIMEleSS project\nhttp://timeless.texture.rocks
 """, formatter_class=RawTextHelpFormatter)
 	
 	# Required arguments
-	parser.add_argument('grainsizelist', help="Path and name for the GrainSize output file")
+	parser.add_argument('grainsizelist', help="Path and name for the file with relative grain sizes")
 	parser.add_argument('-H', '--beamsize_H', required=True, help="Beamsize perpendicular to rotation axis (micrometer), usually horizontal (required)", type=float)
 	parser.add_argument('-V', '--beamsize_V', required=True, help="Beamsize parallel to rotation axis (micrometer), usually vertical (required)", type=float)
 	parser.add_argument('-r', '--rotationrange', required=True, help="Full rotation range. Example: [-28,+28] rotation = 56 degrees (required)", type=float)
@@ -164,8 +168,10 @@ This is part of the TIMEleSS project\nhttp://timeless.texture.rocks
 	parser.add_argument('-i', '--indexquality', required=True, help="Estimate of indexing performance (1.0 is perfect, 0.3 means 30pc of sample grains have been indexed, required)", type=float)
 	
 	# Optionnal arguments
-	parser.add_argument('-rad', '--radius', required=False, help="Add '-rad' to treat the grainsizelist as list of grain radii. Don´t add this argument to treat the grainsizelist as list of grain volumes. Default is volumes.", default=False, action='store_true')
-	parser.add_argument('-hist', '--histogram_bins', required=False, help="If a histogram shall be plotted, give the number of histogram bins here. Default is %(default)s", default=None, type=int)
+	# New file format from previous script returns both relative grain volumes and radii. This was too confusing
+	# parser.add_argument('-rad', '--radius', required=False, help="Add '-rad' to treat the grainsizelist as list of grain radii. Don´t add this argument to treat the grainsizelist as list of grain volumes. Default is volumes.", default=False, action='store_true')
+	# Removed histogram. Not good to mix simple calculations and GUI 
+	# parser.add_argument('-hist', '--histogram_bins', required=False, help="If a histogram shall be plotted, give the number of histogram bins here. Default is %(default)s", default=None, type=int)
 	parser.add_argument('-p', '--proportion', required=False, help="Gives the proportion of the phase of interest relative to the full sample volume. Example: Give 0.3 if your phase of interest makes up only 30 percent of your entire sample. Default is %(default)s.", default=1.0, type=float)
 	
 	# Parse arguments
@@ -176,8 +182,8 @@ This is part of the TIMEleSS project\nhttp://timeless.texture.rocks
 	rotationrange = args['rotationrange']
 	samplethickness = args['samplethickness']
 	indexquality = args['indexquality']
-	radius = args['radius']
-	histogram_bins = args['histogram_bins']
+	# radius = args['radius']
+	# histogram_bins = args['histogram_bins']
 	proportion = args['proportion']
 
 	# check that "proportion" and "indexquality" are below 0 and 1
@@ -189,20 +195,20 @@ This is part of the TIMEleSS project\nhttp://timeless.texture.rocks
 		sys.exit(2)
 		
 
-	grainsizes_R, med = absolute_grainsizes(grainsizelist, beamsize_H, beamsize_V, rotationrange, samplethickness, indexquality, radius=radius, proportion=proportion)
+	grainsizes_R, med = absolute_grainsizes(grainsizelist, beamsize_H, beamsize_V, rotationrange, samplethickness, indexquality, proportion=proportion)
 
 	# Make a histogram
-	if histogram_bins != None:
-		print ("\nPlotting histogram of grain radii ...\n")
-		plt.hist(grainsizes_R, bins = histogram_bins)
-		plt.xscale('log')
-		plt.xlabel("Grain radii ($\mu$m)")
-		plt.ylabel("Number of grains")
-		plt.title("n = %s" % len(grainsizes_R), fontsize = 20)
-		plt.axvline(med, color='k', linestyle='dashed', linewidth=1)
-		ylim_min, ylim_max = plt.ylim()
-		plt.text(med*1.1, ylim_max*0.9, 'Median: {:.4f} $\mu$m'.format(med))
-		plt.show()
+	#if histogram_bins != None:
+		#print ("\nPlotting histogram of grain radii ...\n")
+		#plt.hist(grainsizes_R, bins = histogram_bins)
+		#plt.xscale('log')
+		#plt.xlabel("Grain radii ($\mu$m)")
+		#plt.ylabel("Number of grains")
+		#plt.title("n = %s" % len(grainsizes_R), fontsize = 20)
+		#plt.axvline(med, color='k', linestyle='dashed', linewidth=1)
+		#ylim_min, ylim_max = plt.ylim()
+		#plt.text(med*1.1, ylim_max*0.9, 'Median: {:.4f} $\mu$m'.format(med))
+		#plt.show()
 			
 		
 # Calling method 1 (used when generating a binary in setup.py)
